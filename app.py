34c9f45bb4307f140b11e38a448e34b677152490
houssam <houssam.laghzil@edu.itescia.fr>
2020-05-13 15:59:02 +0200

    end of project

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def __page1__():
-    return render_template("page.html")
-
-
-@app.route('/donner2018.html')
 def __page2__():
     return render_template("donner2018.html")
 
