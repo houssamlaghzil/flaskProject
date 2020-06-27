390b1318e3e7aa53a6dfc3b80a4a2a202c2250f3
houssam <houssam.laghzil@edu.itescia.fr>
2020-06-27 13:53:34 +0200

    final project

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def __page2__():
+@app.route('/donner2018.html')
+def __page1__():
     return render_template("donner2018.html")
+
+
+@app.route('/donner sec.html')
+def __page2__():
+    return render_template("donner sec.html")
 
 
 if __name__ == '__main__':
